c0199816d3aabdcc83fa1c6c241872465a917874
DeltaCircuit <2258588+DeltaCircuit@users.noreply.github.com>
2024-02-21 08:42:53 +0530

    settings init

--- home/programs/waybar/settings.nix
+++ home/programs/waybar/settings.nix
@@ -48,6 +48,53 @@ in {
           tray = {
             spacing = 10;
           };
+
+          network = {
+            "format-wifi" = "{essid} ({signalStrength}%)";
+            "tooltip-format" = "{ifname} via {gwaddr}";
+            "format-alt" = "{ifname} {ipaddr}/{cidr}";
+          };
+
+          pulseaudio = {
+            format = "{icon}  {volume}% {format_source}";
+            format-source = " {volume}%";
+            format-source-mouted = "";
+            format-icons = {
+              headphone = "";
+              hands-free = "";
+              headset = "";
+              phone = "";
+              portable = "";
+              car = "";
+              default = ["" "" ""];
+            };
+          };
+
+          clock = {
+            format = "{:%F %H:%M %Z}";
+            timezones = [
+              "Asia/Kolkata"
+              "US/Central"
+            ];
+            actions = {
+              on-scroll-up = "tz_up";
+              on-scroll-down = "tz_down";
+            };
+            tooltip-format = "{:%Y-%m-%d}";
+          };
+
+          battery = {
+            design-capacity = true;
+            states = {
+              warning = 30;
+              critical = 15;
+            };
+            format = "{icon}      {capacity}%";
+            format-plugged = "  {capacity}%";
+            format-charging = "  {capacity}%";
+            format-icons = ["" "" "" "" ""];
+            format-alt = "{time}      {icon}";
+          };
         }
       ];
     };
