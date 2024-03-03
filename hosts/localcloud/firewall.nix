67a746b5ffaeda60eb60bad8b512aca40eeb0dce
DeltaCircuit <2258588+DeltaCircuit@users.noreply.github.com>
2024-03-03 11:52:08 +0530

    fix braces

--- hosts/localcloud/firewall.nix
+++ hosts/localcloud/firewall.nix
@@ -3,35 +3,36 @@
     enable = true;
     ruleset = ''
       table inet filter {
-        type filter hook input priority 0; policy drop;
+        chain input {
+          type filter hook input priority 0; policy drop;
 
-        # Allow localhost traffic
-        iifname "lo" accept
+          # Allow localhost traffic
+          iifname "lo" accept
 
-        # Allow established/ related comms
-        ct state related,established accept
+          # Allow established/ related comms
+          ct state related,established accept
 
-        # Allow SSH
-        tcp dport 22 accept
+          # Allow SSH
+          tcp dport 22 accept
 
-        # Allow Syncthing P2P
-        tcp dport 22000 accept
+          # Allow Syncthing P2P
+          tcp dport 22000 accept
 
-        # Allow Syncthing GUI
-        tcp dport 8384 accept
+          # Allow Syncthing GUI
+          tcp dport 8384 accept
 
-        # Drop invalid packets
-        ct state invalid drop
+          # Drop invalid packets
+          ct state invalid drop
 
-        reject
-      }
+          reject
+        }
+        chain forward {
+          type filter hook forward priority 0; policy drop;
+        }
 
-      chain forward {
-        type filter hook forward priority 0; policy drop;
-      }
-
-      chain output {
-        type filter hook output priority 0; policy accept;
+        chain output {
+          type filter hook output priority 0; policy accept;
+        }
       }
     '';
   };
