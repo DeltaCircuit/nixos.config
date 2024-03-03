13f178054e2922666132ad14e1768375d9cdb084
DeltaCircuit <2258588+DeltaCircuit@users.noreply.github.com>
2024-03-03 12:13:39 +0530

    replace nftables with iptables

--- hosts/localcloud/firewall.nix
+++ hosts/localcloud/firewall.nix
@@ -1,39 +1,47 @@
+# {
+#   networking.nftables = {
+#     enable = true;
+#     ruleset = ''
+#       table inet filter {
+#         chain input {
+#           type filter hook input priority 0; policy drop;
+#           # Allow localhost traffic
+#           iifname "lo" accept
+#           # Allow established/ related comms
+#           ct state related,established accept
+#           # Allow SSH
+#           tcp dport 22 accept
+#           # Allow Syncthing P2P
+#           tcp dport 22000 accept
+#           # Allow Syncthing GUI
+#           tcp dport 8384 accept
+#           # Drop invalid packets
+#           ct state invalid drop
+#           reject
+#         }
+#         chain forward {
+#           type filter hook forward priority 0; policy drop;
+#         }
+#         chain output {
+#           type filter hook output priority 0; policy accept;
+#         }
+#       }
+#     '';
+#   };
+# }
 {
-  networking.nftables = {
-    enable = true;
-    ruleset = ''
-      table inet filter {
-        chain input {
-          type filter hook input priority 0; policy drop;
+  networking = {
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        22
+        8384
+      ];
 
-          # Allow localhost traffic
-          iifname "lo" accept
-
-          # Allow established/ related comms
-          ct state related,established accept
-
-          # Allow SSH
-          tcp dport 22 accept
-
-          # Allow Syncthing P2P
-          tcp dport 22000 accept
-
-          # Allow Syncthing GUI
-          tcp dport 8384 accept
-
-          # Drop invalid packets
-          ct state invalid drop
-
-          reject
-        }
-        chain forward {
-          type filter hook forward priority 0; policy drop;
-        }
-
-        chain output {
-          type filter hook output priority 0; policy accept;
-        }
-      }
-    '';
+      allowedUDPPorts = [
+        8384
+        22000
+      ];
+    };
   };
 }
