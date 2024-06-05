46b7f60564a341b83167051d24beade80ec7a067
giri <97209964+giri-experion@users.noreply.github.com>
2024-06-05 10:15:35 +0530

    Updates (#2)
    * hp probook config init
    
    * updated lock
    
    * xrander set monitor on lef
    
    * add i3 lock

---
+++ nixos/users/giri.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  ifGroupsExists = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+in {
+  home-manager.users.giri = import ../../hosts/${config.networking.hostName}/giri.nix;
+
+  users.users.giri = {
+    isNormalUser = true;
+    initialPassword = "nixos";
+    description = "giri";
+    shell = pkgs.zsh;
+    extraGroups =
+      [
+        "wheel"
+        "audio"
+        "video"
+        "input"
+      ]
+      ++ ifGroupsExists [
+        "networkmanager"
+        "docker"
+        "git"
+      ];
+    home = "/home/giri";
+    createHome = true;
+
+    packages = [
+      pkgs.home-manager
+    ];
+  };
+
+  programs.zsh.enable = true;
+}
