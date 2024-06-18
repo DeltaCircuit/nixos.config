2e3912dc06ac1b405e89d3055e3743d9a639c272
giri <giridharan.gm@experionglobal.com>
2024-06-18 20:24:33 +0530

    LSP keymaps

--- home/editors/nvim/plugins/lsp.nix
+++ home/editors/nvim/plugins/lsp.nix
@@ -3,6 +3,25 @@
     plugins = {
       lsp = {
         enable = true;
+
+        keymaps = {
+          diagnostic = {
+            "]d" = "goto_next";
+            "[d" = "goto_prev";
+          };
+
+          lspBuf = {
+            K = "hover";
+            gD = "declaration";
+            gd = "definition";
+            gi = "implementation";
+            gt = "type_definition";
+            "<leader>cr" = {
+              action = "rename";
+              desc = "Rename";
+            };
+          };
+        };
       };
     };
   };
