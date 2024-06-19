b7009d9d468ff45052953292b1c7d87440f5b359
DeltaCircuit <2258588+DeltaCircuit@users.noreply.github.com>
2024-06-19 10:59:36 +0530

    basic lsp grammar

--- home/editors/nvim/plugins/treesitter.nix
+++ home/editors/nvim/plugins/treesitter.nix
@@ -1,9 +1,18 @@
-_: {
+{config, ...}: {
   programs.nixvim = {
     plugins = {
       treesitter = {
         enable = true;
         indent = true;
+
+        grammarPackages = with config.programs.nixvim.plugins.treesitter.package.builtGrammars; [
+          vim
+          vimdoc
+          bash
+          diff
+          json
+          toml
+        ];
       };
     };
   };
