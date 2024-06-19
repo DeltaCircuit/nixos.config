5fe604f25006f7e2f21cf661cc247a5fb2705679
DeltaCircuit <2258588+DeltaCircuit@users.noreply.github.com>
2024-06-19 10:13:01 +0530

    format python with black

--- home/editors/nvim/plugins/lsps/python.nix
+++ home/editors/nvim/plugins/lsps/python.nix
@@ -1,8 +1,24 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   programs.nixvim = {
     plugins = {
       dap.extensions.dap-python.enable = true;
       lsp.servers.pyright.enable = true;
+
+      conform-nvim = {
+        formattersByFt = {
+          py = ["black"];
+        };
+
+        formatters = {
+          black = {
+            command = "${pkgs.black}/bin/black";
+          };
+        };
+      };
 
       treesitter = {
         grammarPackages = with config.programs.nixvim.plugins.treesitter.package.builtGrammars; [
