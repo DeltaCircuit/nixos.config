fd963bfac5273a99114e26d9bb7cd647bbf44fce
DeltaCircuit <2258588+DeltaCircuit@users.noreply.github.com>
2024-07-01 10:08:50 +0530

    pull waybar config from external dotfiles

--- home/programs/waybar/default.nix
+++ home/programs/waybar/default.nix
@@ -1,15 +1,12 @@
 {
   lib,
   config,
+  inputs,
   ...
 }:
 with lib; let
   cfg = config.modules.programs.waybar;
 in {
-  imports = [
-    ./settings.nix
-  ];
-
   options.modules.programs.waybar = {
     enable = mkEnableOption "Enable Waybar";
   };
@@ -17,9 +14,10 @@ in {
   config = mkIf cfg.enable {
     programs.waybar = {
       enable = true;
+      settings.mainBar = builtins.fromJSON (builtins.readFile "${inputs.dotfiles}/waybar/config");
+      style = builtins.readFile "${inputs.dotfiles}/waybar/style.css";
       systemd.enable = true;
       systemd.target = "graphical-session-target";
-      style = ./style.css;
     };
   };
 }
