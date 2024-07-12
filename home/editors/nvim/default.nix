aa4fe1a5c7567644186e25eac1c1217ed09ddba5
DeltaCircuit <2258588+DeltaCircuit@users.noreply.github.com>
2024-07-12 12:19:59 +0530

    neovim config

--- home/editors/nvim/default.nix
+++ home/editors/nvim/default.nix
@@ -9,14 +9,6 @@ with lib;
 with inputs; let
   cfg = config.modules.editors.nvim;
 in {
-  #imports = [
-  #  nixvim.homeManagerModules.nixvim
-  #  ./options.nix
-  #  ./plugins
-  #  ./colorschems/catppuccin_mocha.nix
-  #  ./startup.nix
-  #];
-
   options.modules.editors.nvim = with types; {
     enable = mkEnableOption "enable neovim";
   };
@@ -24,14 +16,10 @@ in {
   config = mkIf cfg.enable {
     programs.neovim = {
       enable = true;
-      # defaultEditor = true;
-      #extraPlugins = with pkgs.vimPlugins; [plenary-nvim];
-
-      # extraConfigLua = builtins.readFile ./reload.lua;
     };
 
     xdg.configFile.nvim = {
-      source = "${inputs.hl-nvim-config}";
+      source = "${inputs.dotfiles}/nvim";
       recursive = true;
     };
   };
